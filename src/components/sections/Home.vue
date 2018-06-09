4cbad8dd1ad66b63d05d7d36817aa714cf21d521
Damien <damien.jaillot@gmail.com>
2018-06-09 23:58:56 +0200

    gradient animation

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -2,10 +2,25 @@
     <section class="hero">
         <div class="columns">
             <div class="column is-7-desktop is-8-tablet is-10-mobile">
-                <img class="img-back" src="/images/svg/element/circle_background.svg" alt="circle gradient">
+                <svg id="Circle_background" data-name="Circle Background" viewBox="0 0 1190 964">
+                    <defs>
+                        <linearGradient id="gradient" x1="-318.57" y1="-456.68" x2="862.41" y2="697.04" gradientUnits="userSpaceOnUse">
+                            <stop offset="0" stop-color="#6c00aa">
+                                <animate attributeName="stop-color" values="#6c00aa; #c486b7; #6c00aa" dur="4s" repeatCount="indefinite"></animate>
+                            </stop>
+                            <stop offset="0.55" stop-color="#c486b7">
+                                <animate attributeName="stop-color" values="#c486b7; #e8bcbc; #c486b7" dur="4s" repeatCount="indefinite"></animate>
+                            </stop>
+                            <stop offset="1" stop-color="#e8bcbc">
+                                <animate attributeName="stop-color" values="#e8bcbc; #c486b7; #e8bcbc" dur="4s" repeatCount="indefinite"></animate>
+                            </stop>
+                        </linearGradient>
+                    </defs>
+                    <path class="circle" d="M1169.49,0H0V851.58A781.26,781.26,0,0,0,405,964c433.54,0,785-351.46,785-785A787.32,787.32,0,0,0,1169.49,0Z"/>
+                </svg>
             </div>
         </div>
-        <img class="img-face" src="/images/svg/element/portrait.svg" alt="portrait">
+        <img class="img-face" src="/images/svg/element/portrait.svg" />
     </section>
 </template>
 
@@ -16,7 +31,11 @@
 </script>
 
 <style lang="sass" scoped>
+
     .hero
+        svg
+            .circle
+                fill:url(#gradient)
         .img
             &-face
                 position: absolute
