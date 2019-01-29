53c3f8a4dfc58a7d30832e1c6739e69e32d50064
Damien <damien.jaillot@gmail.com>
2019-01-29 15:46:30 +0100

    fixed home

--- src/components/sections/Home.vue
+++ src/components/sections/Home.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="container">
+    <div class="container has-margin-y-12">
         <div class="columns">
-            <div class="column is-5 is-offset-1">
+            <div class="column has-text-centered is-5 is-offset-1">
                 <img class="img-face" src="/images/svg/element/portrait.svg" />
             </div>
-            <div class="column is-3 is-offset-1"></div>
+            <div class="column is-3 is-mobile is-offset-1"></div>
             <div class="column is-1 is-offset-1"></div>
         </div>
     </div>
@@ -27,15 +27,9 @@
                 left: 20%
                 transition: all 0.2s
                 z-index: 5
-                @media only screen and (max-width: 968px)
-                    left: 10%
-                @media only screen and (min-width: 1145px)
-                    left: 30%
                 @media only screen and (max-width: 650px)
-                    left: 10%
                     max-width: 250px
                 @media only screen and (max-width: 430px)
-                    left: 5%
                     max-width: 200px
                 @media only (min-width: 650px) and (max-height: 690px)
                     max-width: 320px
