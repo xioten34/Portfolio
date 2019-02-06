aeebfd456f1902677c809619fecb3a337ac03103
Damien <damien.jaillot@gmail.com>
2019-02-06 16:13:30 +0100

    fixed carrousel & styling

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -17,19 +17,22 @@
             <div class="column is-5">
                 <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">What i know <span class="has-text-corail">&</span> what i use<span class="has-text-corail">.</span></div>
                 <div class="columns is-multiline is-mobile has-padding-2 has-padding-right-0">
-                    <div v-for="skill in skills" :key="skill.id" class="column is-2-desktop is-one-quarter-mobile is-one-third-tablet has-text-centered">
+                    <div v-for="skill in skills" :key="skill.id" class="column is-2-desktop is-one-quarter-mobile is-one-third-tablet">
                         <figure class="image is-clipped is-square">
                             <img class="has-background-corail" src="" alt="">
                         </figure>
                     </div>
                 </div>
                 <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">Where i used it <span class="has-text-corail">&</span> where i improved it<span class="has-text-corail">.</span></div>
-                <div class="columns is-multiline is-mobile has-padding-2">
-                    <a v-for="exp in experiences" :key="exp.id" class="column is-2-desktop is-one-quarter-mobile is-one-third-tablet has-text-centered">
-                        <figure class="image is-clipped is-square">
-                            <img class="has-background-corail" src="" alt="">
-                        </figure>
-                    </a>
+                <div class="columns is-mobile has-padding-2 has-padding-right-0 is-clipped has-padding-left-20px">
+                    <hooper  :settings="hooperSettings">
+                        <slide v-for="exp in experiences" :key="exp.id" class="has-background-corail">
+                            <h3 class="has-padding-left-1">
+                                Name
+                            </h3>
+                        </slide>
+                        <hooper-navigation slot="hooper-addons"></hooper-navigation>
+                    </hooper>
                 </div>
             </div>
             <div class="column is-1 is-offset-1"></div>
@@ -38,9 +41,19 @@
 </template>
 
 <script>
+    import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper';
+
     export default {
+        components: {
+            Hooper,
+            Slide,
+            HooperNavigation
+        },
         data() {
             return {
+                hooperSettings: {
+                    itemsToShow: 1.5,
+                },
                 skills: [
                     {
                         name: 'HTML',
@@ -98,6 +111,9 @@
                     {
                         name: 'eCare',
                     },
+                    {
+                        name: 'Freelance',
+                    }
                 ]
             }
         }
