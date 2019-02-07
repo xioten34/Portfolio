da66cd3f0e4384926e2185119caf227bc43df45e
Damien <damien.jaillot@gmail.com>
2019-02-07 17:36:01 +0100

    add modal & improve about page

---
+++ src/components/Common/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :class="['modal', active ? 'is-active' : '']">
+        <div class="modal-background">
+        
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['active', 'data'],
+    }
+</script>
--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,59 +1,66 @@
 <template>
-    <div class="container has-margin-y-12">
-        <div class="columns">
-            <div class="column is-5 is-flex">
-                <section class="has-fullheight">
-                    <div>
-                        <span class="has-text-weight-semibold has-text-grey-light">About me<span class="has-text-corail">.</span></span>
-                        <h2 class="title has-text-weight-bold has-margin-top-2">
-                            I love webdesign<br><span class="has-text-corail">&</span> also front-end design<span class="has-text-corail">.</span>
-                        </h2>
-                        <h2 class="subtitle is-size-3 has-text-weight-light has-text-corail">
-                            Thinking, Loving & Making<br>Webdesign
-                        </h2>
+    <section>
+        <modal v-show="isModalActive" :active="isModalActive" :data="experiences" @close="isModalActive = false"></modal>
+        <div class="container has-margin-y-12">
+            <div class="columns">
+                <div class="column is-5 is-flex">
+                    <section class="has-fullheight">
+                        <div>
+                            <span class="has-text-weight-semibold has-text-grey-light">About me<span class="has-text-corail">.</span></span>
+                            <h2 class="title has-text-weight-bold has-margin-top-2">
+                                I love webdesign<br><span class="has-text-corail">&</span> also front-end design<span class="has-text-corail">.</span>
+                            </h2>
+                            <h2 class="subtitle is-size-3 has-text-weight-light has-text-corail">
+                                Thinking, Loving & Making<br>Webdesign
+                            </h2>
+                        </div>
+                    </section>
+                </div>
+                <div class="column is-5">
+                    <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">What i know <span class="has-text-corail">&</span> what i use<span class="has-text-corail">.</span></div>
+                    <div class="columns is-multiline is-mobile has-padding-2 has-padding-right-0">
+                        <div v-for="skill in skills" :key="skill.id" class="column is-2-desktop is-one-quarter-mobile is-one-third-tablet">
+                            <figure class="image is-clipped is-square">
+                                <img class="has-background-corail" src="" alt="">
+                            </figure>
+                        </div>
                     </div>
-                </section>
-            </div>
-            <div class="column is-5">
-                <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">What i know <span class="has-text-corail">&</span> what i use<span class="has-text-corail">.</span></div>
-                <div class="columns is-multiline is-mobile has-padding-2 has-padding-right-0">
-                    <div v-for="skill in skills" :key="skill.id" class="column is-2-desktop is-one-quarter-mobile is-one-third-tablet">
-                        <figure class="image is-clipped is-square">
-                            <img class="has-background-corail" src="" alt="">
-                        </figure>
+                    <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">Where i used it <span class="has-text-corail">&</span> where i improved it<span class="has-text-corail">.</span></div>
+                    <div class="columns is-mobile has-padding-2 has-padding-right-0 is-clipped has-padding-left-20px">
+                        <hooper  :settings="hooperSettings">
+                            <slide v-for="exp in experiences" :key="exp.id" class="has-background-corail">
+                                <h3 class="has-padding-left-1">
+                                    {{ exp.name }}
+                                </h3>
+                                <a @click="isModalActive = true">More</a>
+                            </slide>
+                            <hooper-navigation slot="hooper-addons"></hooper-navigation>
+                        </hooper>
                     </div>
                 </div>
-                <div class="has-padding-left-2 has-text-weight-semibold has-text-grey-dark">Where i used it <span class="has-text-corail">&</span> where i improved it<span class="has-text-corail">.</span></div>
-                <div class="columns is-mobile has-padding-2 has-padding-right-0 is-clipped has-padding-left-20px">
-                    <hooper  :settings="hooperSettings">
-                        <slide v-for="exp in experiences" :key="exp.id" class="has-background-corail">
-                            <h3 class="has-padding-left-1">
-                                Name
-                            </h3>
-                        </slide>
-                        <hooper-navigation slot="hooper-addons"></hooper-navigation>
-                    </hooper>
-                </div>
+                <div class="column is-1 is-offset-1"></div>
             </div>
-            <div class="column is-1 is-offset-1"></div>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
     import { Hooper, Slide, Navigation as HooperNavigation } from 'hooper';
+    import Modal from '../Common/Modal.vue';
 
     export default {
         components: {
             Hooper,
             Slide,
-            HooperNavigation
+            HooperNavigation,
+            Modal
         },
         data() {
             return {
                 hooperSettings: {
                     itemsToShow: 1.5,
                 },
+                isModalActive: false,
                 skills: [
                     {
                         name: 'HTML',
